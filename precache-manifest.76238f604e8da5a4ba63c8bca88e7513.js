f138f49c6fee99691da0adc2a9978665661b8281
Danielle Moss <dani.code.camp@gmail.com>
2018-12-26 17:24:48 -0500

    Updates

---
+++ precache-manifest.76238f604e8da5a4ba63c8bca88e7513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "781b267d9abf92e11aca",
+    "url": "/react-text-generator/static/js/runtime~main.781b267d.js"
+  },
+  {
+    "revision": "9c935eca4fc639d7c782",
+    "url": "/react-text-generator/static/js/main.9c935eca.chunk.js"
+  },
+  {
+    "revision": "0ed3ac256ec96df79b28",
+    "url": "/react-text-generator/static/js/1.0ed3ac25.chunk.js"
+  },
+  {
+    "revision": "9c935eca4fc639d7c782",
+    "url": "/react-text-generator/static/css/main.dec20ccc.chunk.css"
+  },
+  {
+    "revision": "5f6724c47bb4eb2e24eb840d9ef8f168",
+    "url": "/react-text-generator/index.html"
+  }
+];
