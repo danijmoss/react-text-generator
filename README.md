618cb84d1ba4fe0ae1631fa997dde4b1e9d99c3c
Danielle Moss <dani.code.camp@gmail.com>
2018-12-26 17:18:47 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+#ReactJS Sample Text Generator
+
+An app that creates paragraph text with or without the HTML paragraph tag (<p>). This app was made following the [Build & Deploy A React JS Text Generator App tutorial](https://www.youtube.com/watch?v=yU5DYccb77A&t=296s) on the YouTube channel [Traversy Media](https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA).
+
+The app was using [React](https://reactjs.org/), [Axios](https://www.npmjs.com/package/axios), the [Minty theme](https://bootswatch.com/minty/) from [Bootswatch](https://bootswatch.com/), and the [Bacon Ipsum JSON API](https://baconipsum.com/json-api/).
+
+---
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Output from "./Components/Output";
+import Select from "./Components/Controls/Select";
+import Text from "./Components/Controls/Text";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paras: 4,
+      format: "html",
+      text: ""
+    };
+  }
+
+  componentWillMount() {
+    this.getSampleText();
+  }
+
+  getSampleText() {
+    axios
+      .get(
+        "https://baconipsum.com/api/?type=all-meat&paras=" +
+          this.state.paras +
+          "&format=" +
+          this.state.format
+      )
+      .then(response => {
+        this.setState({ text: response.data }, function() {
+          console.log(this.state);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  showHtml(x) {
+    this.setState({ format: x }, this.getSampleText);
+  }
+
+  changeParas(number) {
+    this.setState({ paras: number }, this.getSampleText);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App container'>
+        <h1 className='text-center'>ReactJS Sample Text Generator</h1>
+        <hr />
+        <form className='form-inline'>
+          <div className='form-group'>
+            <label>Paragraphs: </label>
+            <Text
+              value={this.state.paras}
+              onChange={this.changeParas.bind(this)}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Include HTML: </label>
+            <Select
+              value={this.state.format}
+              onChange={this.showHtml.bind(this)}
+            />
+          </div>
+        </form>
+        <br />
+        <br />
+        <Output value={this.state.text} />
       </div>
     );
   }
